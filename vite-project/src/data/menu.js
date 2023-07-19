3ea9486f4a60bbad0d893e4f2ec188c9e520f694
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2023-07-19 16:41:40 +0600

    menu dynamic done

---
+++ vite-project/src/data/menu.js
@@ -0,0 +1,38 @@
+const headerMenu = [
+  {
+    title: "Home",
+    url: "/home",
+  },
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Services",
+    url: "/services",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
+
+const footerMenuOne = [
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Company",
+    url: "/company",
+  },
+  {
+    title: "Community",
+    url: "/community",
+  },
+  {
+    title: "Careers",
+    url: "/careers",
+  },
+];
+export { headerMenu, footerMenuOne };
