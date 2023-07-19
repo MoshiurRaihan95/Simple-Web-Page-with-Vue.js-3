fe7fd0533549f29aff566e1c21ae4cabf35dc0f3
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2023-07-19 12:51:56 +0600

    started work for vue

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,60 +1,48 @@
 <script setup>
  import { data, images, card } from './data/data.js';
- const bgOrange = "bg-orange-400"
- const bgIndigo = "bg-indigo-400"
- const bgGreen = "bg-green-400"
- const textWhite = "text-white text-2xl"
- const textCenter = "flex items-center justify-center"
- const bgRed = "bg-red-600" 
- const bgDrakGreen = "bg-green-600" 
-
-
- const weekend = true
+ 
 </script>
 
 
 
 <template>
-  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-    <h1 class="text-4xl text-black">{{ data.message }}</h1>
-    <h1 class="mt-10 text-xl text-gray-700">Today is <span >{{ data.gateCurrentDate() }}</span></h1>
-    <h1 class="mt-10 text-2xl text-black-700">You have <span class="bg-slate-400 text-orange-600 p-1 rounded-lg">{{ data.task[6]}}</span> task for today.</h1>
-    
-    <section class="container mx-auto p-10">
-      <h1 class="text-4xl mb-10 text-left">Data Project</h1>
-        <section class="main flex space-x-4">
-          <div class="flex space-y-4 flex-col w-1/2">
-            <img :title="images[0].title" :src="images[0].image" alt="">
-            <h2 class="text-xl text-left">{{ images[0].title }}</h2>
-            <a target="_blank" :href="images[0].link" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center">
-              Image Source
-            </a>
-          </div>
-        </section>
-    </section>
-    <section class="container mx-auto">
-      <h1 class="text-center text-2xl py-10">Class & Style Binding</h1>
-
-      <section class="flex space-x-10">
-        <div class="w-1/3 h-16" :class="[bgOrange,textWhite,textCenter]">Hellllo</div>
-        <div class="w-1/3 h-16" :class="bgIndigo"></div>
-        <div class="w-1/3 h-16" :class="bgGreen"></div>
-      </section>
-
-      <section class="mt-10">
-        <div class="h-16 flex items-center justify-center text-white" :class="weekend==true?bgDrakGreen:bgRed">
-          It's a weekday!
-        </div>
-      </section>
-
-      <section class="mt-10 hidden">
-        <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
-          <p class="font-bold">Our privacy policy has changed</p>
-          <p class="text-sm">Make sure you know how these changes affect you.</p>
-        </div>
-      </section>
-    </section>
+ <div>
+  <div class="top_bar">
+    <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="https://flowbite.com/" class="flex items-center">
+          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      </a>
+      <div class="flex md:order-2">
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+          <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>
+      </div>
+      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </nav>
   </div>
+ </div>
   
 </template>
 
